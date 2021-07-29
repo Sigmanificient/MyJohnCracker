8151f75201a97d70aec76b5cbab31ba441a18453
Yohann Boniface <edhyjox@gmail.com>
2021-07-29 23:33:33 +0200

    basic python refactor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-""" archive is an simple python script to compare multiple hash with a dictionary of word """
-# python ~= 3.9
+"""An python script to compare multiple hash with a dictionary of word."""
 
 from time import perf_counter
 from hashlib import sha256
@@ -23,10 +22,16 @@ class Main:
        / / / /___ ______/ /__     /   | _________ _____/ /__  ____ ___  __  __
       / /_/ / __ `/ ___/ //_/    / /| |/ ___/ __ `/ __  / _ \\/ __ `__ \\/ / / /
      / __  / /_/ / /__/ ,<      / ___ / /__/ /_/ / /_/ /  __/ / / / / / /_/ /
-    /_/ /_/\\__,_/\\___/_/|_|    /_/  |_\\___/\\__,_/\\__,_/\\___/_/ /_/ /_/\\__, /
-                                                                     /____/"""
+    /_/ /_/\\__,_/\\___/_/|_|    /_/  |_\\___/\\__,_/\\__,_/\\___/_/ /_/ /_/\\__
+                                                                     /____/
+    """
 
-    def __init__(self, all_hash: str = "assets/hash", dictionary: str = "assets/dict", *overflow: str):
+    def __init__(
+        self,
+        all_hash: str = "assets/hash",
+        dictionary: str = "assets/dict",
+        *overflow: str
+    ):
         print(self.__doc__)
         if overflow or '--help' in argv:
             print(self)
@@ -36,7 +41,11 @@ class Main:
         self.dictionary: List[str] = self.get_content(dictionary)
         print(' results '.center(32, '-'))
 
-        self.multiple(self.all_hash) if len(self.all_hash) != 1 else self.single(self.all_hash[0])
+        (
+            self.multiple(self.all_hash)
+            if len(self.all_hash) != 1 else self.single(self.all_hash[0])
+        )
+
         input("Press enter key to quit...")
 
     def __repr__(self):
@@ -50,13 +59,10 @@ class Main:
 
     @staticmethod
     def get_hash(string) -> str:
-        """ Warning HL101: MD5, SHA-1, RIPEMD-160, Whirlpool, and the SHA-256 / SHA-512 hash algorithms are all
-        vulnerable to length-extension attacks and should not be used for obfuscating or protecting data. Use within
-        a HMAC is not vulnerable. ( Hashlib Documentation )"""
         return sha256(string.encode()).hexdigest()
 
     def single(self, _hash: str):
-        """ When only one hash is provided, scroll directly within the directory """
+        """If only one hash is provided, scroll within the directory."""
 
         val: str
         for val in self.dictionary:
@@ -67,14 +73,21 @@ class Main:
             self.log("<!> No hash found")
 
     def multiple(self, _hash_list: List[str]):
-        """ When hash file contains multiple lines or multiple files are given.
-        An hash dictionary is built which is way faster than a multiple for loop but ask lots of memory """
-        self.dictionary: dict[str:str] = {self.get_hash(val): val for val in self.dictionary}
+        """When hash file contains multiple lines or multiple files are given.
+
+        An hash dictionary is built which is way faster than a multiple
+        for loop but ask lots of memory."""
+        self.dictionary: dict[str:str] = {
+            self.get_hash(val): val for val in self.dictionary
+        }
 
         _hash: str
         for _hash in _hash_list:
             val: Optional[str] = self.dictionary.get(_hash)
-            self.log("<!> No hash found" if val is None else f"Found matching hash for {val} ({_hash})")
+            self.log(
+                "<!> No hash found"
+                if val is None else f"Found matching hash for {val} ({_hash})"
+            )
 
     @staticmethod
     def log(string: str):
@@ -82,7 +95,7 @@ class Main:
         print(f"[{perf_counter():,.3f}s] {string}")
 
     def get_content(self, _path: str) -> List[str]:
-        """ Get all values from a path and merge file if a directory is given """
+        """.Get all values from path and merge file if a directory is given."""
         if path.isfile(_path):
             print(f"Opening file: {_path}")
             with open(_path) as f:
